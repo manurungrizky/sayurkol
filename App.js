c2fb8ec02238023be2cd901982ce3de6428f6150
rizkymanurung <rizkymanurung11@gmail.com>
2018-12-14 11:26:30 -0800

    first commit

---
+++ App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Setup from "./src/boot/setup";
+import Footer from "./src/screens/footer";
+export default class App extends React.Component {
+  render() {
+    return <Setup/>;
+    // return <Footer/>;
+  }
+}
---
+++ src/boot/setup.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Container, Button, Text } from 'native-base';
+export default class App extends Component {
+  render() {
+    return (
+      <Container>
+        <Button>
+          <Text>
+            ini hanya button contoh
+          </Text>
+        </Button>
+      </Container>
+    );
+  }
+}
